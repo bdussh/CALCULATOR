075ed9f9a5081d3b479b2680d07583095327dd36
bdussh <dbidyc@gmail.com>
2022-07-06 15:27:35 -0500

    structured script.js

--- script.js
+++ script.js
@@ -41,103 +41,121 @@ function operate(a, b, operator) {
     }
 }
 
-function restart() {
+function display() {
+    operation.textContent = `${memory.firstOperand} ${memory.operator} ${memory.secondOperand}`
+    if (i1 === true) {
+        number.textContent = memory.firstOperand
+    } else if (i2 === true) {
+        number.textContent = memory.secondOperand
+    } else {
+        number.textContent = memory.operator
+    }
+}
+
+function buttonClear() {
     memory.firstOperand = ""
     memory.secondOperand = ""
     memory.operator = ""
     i1 = true
     io = false
     i2 = false
-    number.textContent = ''
-    operation.textContent = ''
+    display()
+}
+
+
+function buttonDigit(button) {
+    if (i1 === true) {
+        memory.firstOperand += button.id
+        display()
+    } else if (i2 === true) {
+        memory.secondOperand += button.id
+        display()
+    } else {
+        io = false
+        i2 = true
+        memory.secondOperand += button.id
+        display()
+    }
+}
+
+function buttonOperand(button) {
+    if (i1 === true && memory.firstOperand !== '') {
+        i1 = false
+        io = true
+        memory.operator = button.id
+        display()
+    } else if (i2 === true) {
+        i2 = false
+        io = true
+        memory.firstOperand = operate(memory.firstOperand, memory.secondOperand, memory.operator).toString()
+        memory.secondOperand = ''
+        memory.operator = button.id
+        display()
+    } else {
+        memory.operator = button.id
+        memory.secondOperand = ''
+        display()
+
+    }
+}
+
+function buttonEqual() {
+    if (i2 === true) {
+
+        operation.textContent = `${memory.firstOperand} ${memory.operator} ${memory.secondOperand} =`
+        memory.firstOperand = operate(memory.firstOperand, memory.secondOperand, memory.operator).toString()
+        number.textContent = memory.firstOperand
+        memory.secondOperand = ''
+        memory.operator = ''
+        i2 = false
+        i1 = true
+
+    }
+}
+
+function buttonDelete() {
+    if (i1 === true) {
+        memory.firstOperand = memory.firstOperand.slice(0, -1)
+        display()
+    } else if (i2 === true) {
+        memory.secondOperand = memory.secondOperand.slice(0, -1)
+        display()
+    }
+}
+
+function buttonPoint() {
+    if (i1 === true) {
+        if (memory.firstOperand === '') {
+            memory.firstOperand = '0.'
+        } else if (!memory.firstOperand.includes('.')) {
+            memory.firstOperand += '.'
+        }
+        display()
+
+    } else if (i2 === true) {
+        if (memory.secondOperand === '') {
+            memory.secondOperand = '0.'
+        } else if (!memory.secondOperand.includes('.')) {
+            memory.secondOperand += '.'
+        }
+        display()
+    }
 }
 
 function logic(button) {
-
     if (button.id === "clear") {
-        restart()
+        buttonClear()
     } else if (button.id in digits) {
-        if (i1 === true) {
-            memory.firstOperand += button.id
-            number.textContent = memory.firstOperand
-            operation.textContent = memory.firstOperand
-        } else if (i2 === true) {
-            memory.secondOperand += button.id
-            number.textContent = memory.secondOperand
-            operation.textContent = `${memory.firstOperand} ${memory.operator} ${memory.secondOperand}`
-        } else {
-            io = false
-            i2 = true
-            memory.secondOperand += button.id
-            number.textContent = memory.secondOperand
-            operation.textContent = `${memory.firstOperand} ${memory.operator} ${memory.secondOperand}`
-        }
+        buttonDigit(button)
     } else if (button.id === "+" || button.id === "-" || button.id === "×" || button.id === "÷") {
-        if (i1 === true && memory.firstOperand !== '') {
-            i1 = false
-            io = true
-            memory.operator = button.id
-            number.textContent = memory.operator
-            operation.textContent = `${memory.firstOperand} ${memory.operator}`
-        } else if (i2 === true) {
-            i2 = false
-            io = true
-            memory.firstOperand = operate(memory.firstOperand, memory.secondOperand, memory.operator).toString()
-            memory.secondOperand = ''
-            memory.operator = button.id
-            number.textContent = memory.operator
-            operation.textContent = memory.firstOperand
-        } else {
-            memory.operator = button.id
-            number.textContent = memory.operator
-            memory.secondOperand = ''
-            operation.textContent = `${memory.firstOperand} ${memory.operator}`
-
-        }
+        buttonOperand(button)
     } else if (button.id === '=') {
-        if (i2 === true) {
-
-            operation.textContent = `${memory.firstOperand} ${memory.operator} ${memory.secondOperand} =`
-            memory.firstOperand = operate(memory.firstOperand, memory.secondOperand, memory.operator).toString()
-            number.textContent = memory.firstOperand
-            memory.secondOperand = ''
-            memory.operator = ''
-            i2 = false
-            i1 = true
-
-        }
+        buttonEqual(button)
     } else if (button.id === 'delete') {
-        if (i1 === true) {
-            memory.firstOperand = memory.firstOperand.slice(0, -1)
-            number.textContent = memory.firstOperand
-            operation.textContent = memory.firstOperand
-        } else if (i2 === true) {
-            memory.secondOperand = memory.secondOperand.slice(0, -1)
-            number.textContent = memory.secondOperand
-            operation.textContent = `${memory.firstOperand} ${memory.operator} ${memory.secondOperand}`
-        }
+        buttonDelete()
     } else if (button.id === 'point') {
-        if (i1 === true) {
-            if (memory.firstOperand === '') {
-                memory.firstOperand = '0.'
-            } else if (!memory.firstOperand.includes('.')) {
-                memory.firstOperand += '.'
-            }
-            number.textContent = memory.firstOperand
-            operation.textContent = memory.firstOperand
-
-        } else if (i2 === true) {
-            if (memory.secondOperand === '') {
-                memory.secondOperand = '0.'
-            } else if (!memory.secondOperand.includes('.')) {
-                memory.secondOperand += '.'
-            }
-            number.textContent = memory.secondOperand
-            operation.textContent = `${memory.firstOperand} ${memory.operator} ${memory.secondOperand}`
-        }
+        buttonPoint()
     }
-    console.log(memory)
-    console.log(i1, io, i2)
 }
 
 buttons.forEach(function (button) {
